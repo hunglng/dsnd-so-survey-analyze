74a804d05581d6186be2b9fc92ddfcd82513c1b6
Le Nguyen Gia Hung <hunglng2203@gmail.com>
2023-03-15 03:35:57 +0700

    Change question 1  to per

--- split_multiple_choice.py
+++ split_multiple_choice.py
@@ -16,3 +16,20 @@ def create_dataframe(df, ds, name):
     df = pd.concat([df, count_ds], axis=1, join="outer")
 
     return df
+
+def create_dataframe_v2(df, ds, name):
+    '''
+    INPUT
+        df - Data Frame
+        ds - Data Series
+        name - Column Name for new data series
+    OUTPUT
+        return newly concat data frame
+    '''
+    s = ds.shape[0]
+    count_ds = ds.str.split(pat=';').explode().str.strip().str.upper().value_counts()
+    count_ds.rename(name, inplace=True)
+    count_ds = count_ds.apply(lambda x: x/s)
+    df = pd.concat([df, count_ds], axis=1, join="outer")
+
+    return df
